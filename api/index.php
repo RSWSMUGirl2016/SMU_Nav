f4243420079e2aab3ad225e66972801aafd1ac8e
Drew Fulsom <afulsom@smu.edu>
2014-10-28 10:24:29 -0500

    createUserAccount fixture

--- api/index.php
+++ api/index.php
@@ -4,5 +4,19 @@ $app = new \Slim\Slim();
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
+
+
+$app->post('/createUserAccount', function () {
+    $dummyJSON = array ('u_id'=>1);
+    $fName = $_POST['fName'];
+    $lName = $_POST['lName'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    echo json_encode($dummyJSON);
+});
+
+
+
 $app->run();
 ?>
