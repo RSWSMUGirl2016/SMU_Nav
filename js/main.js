e513b67abccf276af1a9543213e8dd7ddbaafe95
RSWSMUGirl2016 <rsward@smu.edu>
2014-11-21 20:41:54 -0600

    New way of searching for locations on campus only

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	
-    var markers = [];
+    var marker;
     var map;
     function initializeMap() {
         var mapOptions = {
@@ -11,89 +11,10 @@ $(document).ready(function () {
 
         map = new google.maps.Map(document.getElementById('mapWrapper'), mapOptions);
 
-          // Create the search box and link it to the UI element.
-          var input = (document.getElementById('map-input'));
-          map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+        var buildingName;
+        var roomName;
+        var roomNumber;
 
-          var searchBox = new google.maps.places.SearchBox(input);
-
-          // [START region_getplaces]
-          // Listen for the event fired when the user selects an item from the
-          // pick list. Retrieve the matching places for that item.
-          google.maps.event.addListener(searchBox, 'places_changed', function() {
-            var places = searchBox.getPlaces();
-
-            if (places.length == 0) {
-              return;
-            }
-            for (var i = 0, marker; marker = markers[i]; i++) {
-              marker.setMap(null);
-            }
-
-            // For each place, get the icon, place name, and location.
-            markers = [];
-            var bounds = new google.maps.LatLngBounds();
-            for (var i = 0, place; place = places[i]; i++) {
-              var image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-              };
-
-              // Create a marker for each place.
-              var marker = new google.maps.Marker({
-                map: map,
-                icon: image,
-                title: place.name,
-                position: place.geometry.location
-              });
-
-              markers.push(marker);
-
-              google.maps.event.addListener(map, 'center_changed', function() {
-                // 3 seconds after the center of the map has changed, pan back to the
-                // marker.
-                window.setTimeout(function() {
-                  map.panTo(marker.getPosition());
-                }, 3000);
-              });
-
-              var contentString = '<div id="content">'+
-                  '<div id="siteNotice">'+
-                  '</div>'+
-                  '<div id="bodyContent">'+
-                  '<h6 id="firstHeading" class="favoritesHeading">Add to Favorites</h6>'+
-                  '<button id="favorites_bttn">Add</button>'+
-                  '<h6 id="getDirectionsHeading" class="firstHeading">Get Directions</h6>'+
-                  '<button id="getDirections_bttn">Start</button>'+
-                  '</div>'+
-                  '</div>';
-
-              var infowindow = new google.maps.InfoWindow({
-                  content: contentString
-              });
-
-              google.maps.event.addListener(marker, 'click', function() {
-                //map.setZoom(8);
-                //map.setCenter(marker.getPosition());
-                infowindow.open(map, marker);
-              });
-
-              bounds.extend(place.geometry.location);
-            }
-
-            map.fitBounds(bounds);
-          });
-          // [END region_getplaces]
-
-          // Bias the SearchBox results towards places that are within the bounds of the
-          // current map's viewport.
-          google.maps.event.addListener(map, 'bounds_changed', function() {
-            var bounds = map.getBounds();
-            searchBox.setBounds(bounds);
-          });
     }
 	google.maps.event.addDomListener(window, 'load', initializeMap);
 
